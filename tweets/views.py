63d9a363455297168ff768978070e73567a10ba6
asuracoder91 <asuracoder@gmail.com>
2024-10-03 00:05:45 +0900

    feat: all tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Tweet
+
 
 # Create your views here.
+def tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "tweets.html",
+        {
+            "tweets": tweets,
+            "title": "Tweets",
+        },
+    )
